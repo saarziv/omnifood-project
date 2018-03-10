ec70a1d63ae5875e78d07603f15c3eb636607c26
saar <saarzivon@gmail.com>
2018-03-10 20:35:44 +0200

    added nodejs server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const port = process.env.PORT;
 app.use(express.static(path.join(__dirname,"../")));
 
 app.get("*",(req,res) =>{
     res.sendFile(`index.html`);
 });
 
-app.listen(3000,() => {
-    console.log("listening on port 3000...");
+app.listen(port,() => {
+    console.log(`listening on port ${port}...`);
 });
